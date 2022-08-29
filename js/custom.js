a6c48dab6053f2c170afcf26f2618ce785a3fb2d
rachad47 <58671681+rachad47@users.noreply.github.com>
2022-08-28 22:38:43 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -36,3 +36,31 @@
     $('.social-links a').tooltip();
 
 })(jQuery);
+
+var playBtn = document.getElementById('play'),
+  resetBtn = document.getElementById('reset'),
+  hearbeat = document.getElementById('heartbeat')
+	audios = document.querySelectorAll('audio');
+console.log(audios);
+
+
+playBtn.addEventListener('mouseover', function() {
+[].forEach.call(audios, function(audio) {
+  // do whatever
+  audio.play();
+});
+}, false);
+
+playBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
+
+resetBtn.addEventListener('mouseover', function() {
+    heartbeat.play();
+}, false);
+
+resetBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
